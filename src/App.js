40b55d658d032fa13d5d6d70946e48e664259a86
jokino92 <punisher9212@mail.ru>
2025-03-11 22:59:20 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css'; // Изменили здесь
+
+function App() {
+  return (
+    <div className="container">
+      <div className="square"></div>
+    </div>
+  );
+}
+
+export default App;
